d2e084e30f831e209c7ea6ceaa450950d00a6869
AlexanderBaranov <avbc@mail.ru>
2024-12-02 17:16:31 +0200

    HW #45

--- src/main/java/employees/CompanyImpl.java
+++ src/main/java/employees/CompanyImpl.java
@@ -2,6 +2,7 @@ package employees;
 
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.concurrent.locks.*;
 import java.lang.IllegalStateException;
 import io.Persistable;
 import java.io.*;
@@ -11,7 +12,9 @@ public class CompanyImpl implements Company, Persistable {
     private HashMap<String, List<Employee>> employeesDepartment = new HashMap<>();
     private TreeMap<Float, List<Manager>> managers = new TreeMap<>();
     private boolean stateChanged = false;
-
+    private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private Lock readLock = readWriteLock.readLock();
+    private Lock writeLock = readWriteLock.writeLock();
     public class IteratorCompany implements Iterator<Employee>{
         Iterator<Employee> it = employees.values().iterator();
         Employee iteratedObj;
@@ -43,14 +46,19 @@ public class CompanyImpl implements Company, Persistable {
 
     @Override
     public void addEmployee(Employee empl) {
-        Long id = empl.getId();
-        if (getEmployee(id) != null) {
-            throw new IllegalStateException("The employee with this id is already exist in th compamy");
+        try {
+            writeLock.lock();
+            Long id = empl.getId();
+            if (getEmployee(id) != null) {
+                throw new IllegalStateException("The employee with this id is already exist in th compamy");
+            }
+            employees.put(id, empl);
+            addEmployeeToDepartment(empl);
+            addEmployeeToManagers(empl);
+            stateChanged = true;
+        } finally {
+            writeLock.unlock();
         }
-        employees.put(id, empl);
-        addEmployeeToDepartment(empl);
-        addEmployeeToManagers(empl);
-        stateChanged = true;
     }
 
     @SuppressWarnings("unused")
@@ -68,19 +76,29 @@ public class CompanyImpl implements Company, Persistable {
 
     @Override
     public Employee getEmployee(long id) {
-        return employees.get(id);
+        try {
+            readLock.lock();
+            return employees.get(id);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
     public Employee removeEmployee(long id) {
-        Employee empl = employees.get(id);
-        if (empl == null) {
-            throw new NoSuchElementException("The employee with this id is not exist in the compamy");
+        try {
+            writeLock.lock();
+            Employee empl = employees.get(id);
+            if (empl == null) {
+                throw new NoSuchElementException("The employee with this id is not exist in the compamy");
+            }
+            removeEmployeeFromDepartment(empl);
+            removeManagerFromManagers(empl);
+            stateChanged = true;
+            return employees.remove(id);
+        } finally {
+            writeLock.unlock();
         }
-        removeEmployeeFromDepartment(empl);
-        removeManagerFromManagers(empl);
-        stateChanged = true;
-        return employees.remove(id);
     }
 
     private void removeEmployeeFromDepartment(Employee empl) {
@@ -103,33 +121,52 @@ public class CompanyImpl implements Company, Persistable {
     }
 
     @Override
-    public int getDepartmentBudget(String department) {
-        int budget = 0;
-        List<Employee> listEmployees = employeesDepartment.get(department);
-        if (listEmployees != null) {
-            budget = listEmployees.stream().mapToInt(e -> e.computeSalary()).sum();
+    public int getDepartmentBudget(String department) { 
+        try {
+            readLock.lock();
+            int budget = 0;
+            List<Employee> listEmployees = employeesDepartment.get(department);
+            if (listEmployees != null) {
+                budget = listEmployees.stream().mapToInt(e -> e.computeSalary()).sum();
+            }
+            return budget;
+        } finally {
+            readLock.unlock();
         }
-        return budget;
     }
 
     @Override
     public String[] getDepartments() {
-        return employeesDepartment.keySet().stream().sorted().toArray(String[]::new);
+        try {
+            readLock.lock();
+            return employeesDepartment.keySet().stream().sorted().toArray(String[]::new);
+        } finally {
+            readLock.unlock();
+        }   
     }
 
     @Override
     public Manager[] getManagersWithMostFactor() {
-        Entry<Float, List<Manager>> mostFactorEntry = managers.lastEntry();
-        Manager[] res = {};
-        if (mostFactorEntry != null) {
-            res = mostFactorEntry.getValue().stream().toArray(Manager[]::new);
+        try {
+            readLock.lock();
+            Entry<Float, List<Manager>> mostFactorEntry = managers.lastEntry();
+            Manager[] res = {};
+            if (mostFactorEntry != null) {
+                res = mostFactorEntry.getValue().stream().toArray(Manager[]::new);
+            }
+            return res;
+        } finally {
+            readLock.unlock();
         }
-        return res;
+
     }
+
 
     @Override
     public boolean saveTofile(String fileName) {
-        boolean res = false;
+        try {
+            readLock.lock();
+            boolean res = false;
         if (stateChanged) {
             try (PrintWriter writer = new PrintWriter(fileName)) {
                 forEach(writer::println);
@@ -140,7 +177,10 @@ public class CompanyImpl implements Company, Persistable {
             }
         }
         return res;
+    } finally {
+        readLock.unlock();
     }
+}
 
     @Override
     public void restoreFromFile(String fileName) {
